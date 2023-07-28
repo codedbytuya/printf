ff3845d0c5fe68185e8de17de576ed012deec104
codedbytuya <133232885+codedbytuya@users.noreply.github.com>
2023-07-28 12:03:29 +0200

    Update _printf.c -Helper function decleration

--- _printf.c
+++ _printf.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+int print_integer(int num); 
+
 /**
  * _printf - Prints formatted output to the standard output stream (stdout).
  * @format: A character string containing format specifiers that control the output.
@@ -16,7 +18,7 @@ int _printf(const char *format, ...)
     int count = 0;
     char c;
     const char *str;
-    int num; // To hold the integer value
+    int num;
 
     while (*format)
     {
@@ -43,7 +45,7 @@ int _printf(const char *format, ...)
                 case 'd':
                 case 'i':
                     num = va_arg(args, int);
-                    // Function to print an integer to stdout
+                    
                     count += print_integer(num);
                     break;
                 case '%':
@@ -72,18 +74,18 @@ int _printf(const char *format, ...)
     return count;
 }
 
-// Helper function to print an integer to stdout
+
 int print_integer(int num)
 {
     int count = 0;
-    char buffer[20]; // Assuming a reasonable buffer size for integer representation
+    char buffer[20];
 
-    // Handle negative numbers
+    
     if (num < 0)
     {
         write(1, "-", 1);
         count++;
-        num = -num; // Convert to positive for easier handling
+        num = -num;
     }
 
     int i = 0;
@@ -93,8 +95,9 @@ int print_integer(int num)
         num /= 10;
     } while (num != 0);
 
-    // Print the digits in reverse order
-    for (int j = i - 1; j >= 0; j--)
+    
+    int j;
+    for (j = i - 1; j >= 0; j--)
     {
         write(1, &buffer[j], 1);
         count++;
